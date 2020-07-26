f8a46d3a5da310a8e57f88e7cf48436e2976ce20
jenchieh <jcs090218@gmail.com>
2020-07-26 15:22:40 +0800

    Add Unity core.

---
+++ UCTooth/Assets/UCTooth/Scripts/Test/Test_General.cs
@@ -0,0 +1,25 @@
+/**
+ * $File: Test_General.cs $
+ * $Date: 2020-07-26 15:04:28 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information 
+ *	                 Copyright © 2020 by Shen, Jen-Chieh $
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class Test_General 
+    : MonoBehaviour 
+{
+    /* Variables */
+
+    /* Setter & Getter */
+
+    /* Functions */
+
+}
